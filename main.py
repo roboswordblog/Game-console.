8b6623e56df44909f7ca275aebc73497e22354af
Robosword <robotanmol@gmail.com>
2026-05-26 17:31:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+from buildhat import Motor
+import pygame
+pygame.init()
+
+window = pygame.display.set_mode((800, 600))
+
+def text():
+  pass
+
+class ControlManagement:
+  def __init__(self):
+    self.motorcontrollerA = Motor("A")
+    self.motorcontrollerB = Motor("B")
+
+class Button:
+  pass
+
+
+class WindowManage:
+  def __init__(self):
+    self.function = None
+
+  def gameChoose(self):
+  pass
+  
+  def pong(self):
+    pass
+
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+  pygame.display.update()
+
 
